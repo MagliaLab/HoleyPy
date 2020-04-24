0e5d795dacda2ba8a051fe881f5bbc19aa41d4c9
flrlucas <flrlucas@gmail.com>
2020-04-24 16:09:07 +0200

    Added tests in test_nanolyse.py Added try/except clause in loadAxon.py for imports

--- test_nanolyse.py
+++ test_nanolyse.py
@@ -1,3 +1,24 @@
 """
 All unittests and integration tests will go here
 """
+import numpy as np
+import nanolyse as nl
+
+__sampling_period = 2e-5
+__test_filter = 2000
+__blank_fname = './data/Blank.csv'
+__sample_fname = './data/ProteinDigest.csv'
+__levels = (True, -115.7, 1.2)
+
+def load_filt( fname ):
+    return nl.filter_gaussian( [np.genfromtxt(__blank_fname, delimiter='')], __sampling_period, __test_filter )
+
+def test_get_levels():
+    blank_array = load_filt(__blank_fname)
+    levels = nl.get_levels(blank_array)
+
+def test_get_events():
+    sample_array = load_filt(__sample_fname)
+    event_data = nl.thresholdsearch(sample_array, __sampling_period, __levels)
+    nl.get_features_THS(event_data, __sampling_period)
+
