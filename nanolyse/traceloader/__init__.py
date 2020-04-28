026763515bf5eac7e09bd2c064596bb30a1710be
flrlucas <flrlucas@gmail.com>
2020-04-28 15:40:06 +0200

    Added traceloader package. Removed loadAxon.py

---
+++ nanolyse/traceloader/__init__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+import os
+
+from . import csvloader
+from . import axonabf
+
+
+_ext_dict = {
+    ".csv": "csvloader",
+    ".abf": "axonabf",
+}
+
+
+def load(filename):
+    """Function to load data traces.
+
+    Dynamically load data traces based on file extension.
+    Support:
+        .csv
+        .abf
+
+    Parameters
+    ----------
+    filename: str
+            File path
+
+    Returns
+    -------
+    list
+            Return list containing data traces
+    """
+    _, ext = os.path.splitext(filename)
+    try:
+        return eval(_ext_dict[ext]).load(filename)
+    except OSError:
+        return False
