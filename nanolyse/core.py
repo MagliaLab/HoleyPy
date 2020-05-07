feb807e06c9008ea8083f4f9f9bec2a57b00cd5e
Matthijs Tadema <M.J.Tadema@protonmail.com>
2020-05-07 15:15:01 +0200

    First draft core object. Will read its data into numpy array so be careful with large files, will fix later.

--- nanolyse/core.py
+++ nanolyse/core.py
@@ -34,15 +34,37 @@ def get_preprocessor(extension, reader):
     """
     raise NotImplementedError
 
-class Core():
+class Core(np.ndarray):
     """
     We need a "core" object, something that takes care of reading the data,
     maybe preprocesses it according to its type, and then presents
     the data to the user in a user-friendly way.
-    Data will be lazily pulled from self._reader
+    Data will be lazily pulled from self._reader.
+    New plan, try to subclass from ndarray
+
+    This will do for loading csv files,
+    we will have to come up with a different solution for
+    hdf5 files
     """
-    def __init__(self, datafile):
-        extension = get_extension(datafile)
-        reader = get_reader(datafile, extension)
-        self._reader = get_preprocessor(extension, reader)
-        pass
+    def __new__(cls, datafile, extension):
+        """
+        This will initialize our object as a subclasses ndarray
+        info @: https://numpy.org/devdocs/user/basics.subclassing.html
+        """
+        loader = get_loader(extension)
+        loader = get_preprocessor(loader, extension)
+        data = loader(datafile)
+        obj = np.asarray(data).view(cls)
+        # Keep the extension and loader just in case
+        obj._extension = extension
+        obj._loader = loader
+        return obj
+
+    def __array_finalize__(self, obj):
+        """
+        Here will be the actual initialization of new attributes
+        """
+        if obj is None: return
+        self._extension = getattr(obj, '_extension', None)
+        self._loader = getattr(obj, '_loader', None)
+
