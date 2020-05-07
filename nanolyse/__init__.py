682f832a4f27852ae838c8a719f4757fdddd54f8
Matthijs Tadema <M.J.Tadema@protonmail.com>
2020-05-07 15:16:07 +0200

    Changed how modules are loaded into the root loader. We want don't want to show internal stuff to the user.

--- nanolyse/__init__.py
+++ nanolyse/__init__.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
-from . import loaders
+#from . import loaders
 
-from .get_levels import *
-from .get_events import *
-from .join_traces import *
-from .get_features_THS import *
-from .thresholdsearch_mmult import *
+#from .get_levels import *
+#from .get_events import *
+#from .join_traces import *
+#from .get_features_THS import *
+#from .thresholdsearch_mmult import *
 
-from .filters import *
+#from .filters import *
 
+from .core import Core
 
 name = "nanolyse"
