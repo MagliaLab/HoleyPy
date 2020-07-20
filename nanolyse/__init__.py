1befe833a059e8a823f4d463c931b429f7296a65
Matthijs Tadema <M.J.Tadema@protonmail.com>
2020-07-20 15:18:07 +0200

    Changed __init__.py for a clearer interface

--- nanolyse/__init__.py
+++ nanolyse/__init__.py
@@ -1,11 +1,2 @@
 # -*- coding: utf-8 -*-
-from . import traceloader
-from .core import *
-
-from .get_levels import *
-from .get_events import *
-from .join_traces import *
-from .get_features_THS import *
-from .thresholdsearch_mmult import *
-
-from .filters import *
+from .trace import Trace
